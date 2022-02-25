5c8c4db115140091f159296d205448ec206f24d1
Nahuel Pedrozo <nahuel502010@gmail.com>
2022-02-24 22:37:25 -0300

    cuarto commit: agrega array

--- js/script.js
+++ js/script.js
@@ -11,14 +11,25 @@ function sumar(importe) {
 }
 
 const registro = new Persona(prompt("Ingrese su nombre"), parseInt(prompt("Ingrese el dinero que dispone")));
+const movimientos = new Array();
 
 let operacion = parseInt(prompt("Desea realizar alguna operación\n 0)-no\n 1) si"));
 while (operacion === 1) {
+  let importe;
   let tipo = parseInt(prompt("Si desea agregar credito ingrese 0 si desea restar ingrese 1"));
   if (tipo === 0) {
-    alert("El saldo que tiene es "+ sumar(parseInt(prompt("Ingrese el valor de la operación realizada"))));
+    importe= parseInt(prompt("Ingrese el valor de la operación realizada"));
+    alert("El saldo que tiene es "+ sumar(importe));
+    movimientos.push(importe);
   } else {
-    alert("El saldo que tiene es"+ restar(parseInt(prompt("Ingrese el valor de la operación realizada"))));
+    importe= prompt( "Ingrese el valor de la operación realizada");
+    alert("El saldo que tiene es "+ restar(importe));
+    movimientos.push(-importe);
   }
+  
   operacion = parseInt(prompt("Si desea continuar ingresando operaciones ingrese 1"));
+}
+
+for(let i=0; i<movimientos.length;  i++ ){
+  alert("El movimiento " + i + "es de $ " + movimientos[i]);
 }
